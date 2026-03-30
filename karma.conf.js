fdc73333d98fd24704baba377bfab5f1f0f69182
Brahem Ahmed <138380756+Brahem-Ahmed@users.noreply.github.com>
2026-03-30 12:00:00 +0100

    "resolving pipeline issues"

--- karma.conf.js
+++ karma.conf.js
@@ -1,17 +1,20 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+const getBaseKarmaConfig = require('@angular-devkit/build-angular/plugins/karma').getBaseKarmaConfig;
+
 module.exports = function (config) {
-  config.set({
-    basePath: '',
+  getBaseKarmaConfig(config, {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('karma-coverage')
     ],
+  });
+  
+  config.set({
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
